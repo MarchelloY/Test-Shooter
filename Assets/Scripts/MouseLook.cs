96bb116392811f2d1b49437da5335c20e185b2b3
Marchenko Maxim <maximm@playtika.com>
2020-10-14 16:52:48 +0300

    updated the project and cleaned up

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -7,20 +7,21 @@ public class MouseLook : MonoBehaviour
 
     [SerializeField] private float mouseSensitivity = 100f;
     [SerializeField] private float maxAngle = 60f;
-    
+
     private float _xRotation;
 
-    private void Start ()
+    private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void FixedUpdate () {
-        var mouseX = Input.GetAxis ("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
-        var mouseY = Input.GetAxis ("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+    private void FixedUpdate()
+    {
+        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
         _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp (_xRotation, -maxAngle, maxAngle);
-        playerHead.localRotation = Quaternion.Euler (_xRotation, 0f, 0f);
-        playerBody.Rotate (Vector3.up * mouseX);
+        _xRotation = Mathf.Clamp(_xRotation, -maxAngle, maxAngle);
+        playerHead.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/Scripts/PersonController.cs
+++ Assets/Scripts/PersonController.cs
@@ -1,52 +1,51 @@
 ﻿using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
 
 public class PersonController : MonoBehaviour
 {
-	
-	[SerializeField] private Transform player1;
-	[SerializeField] private Transform player2;
-	[SerializeField] private Camera playerCamera1;
-	[SerializeField] private Camera playerCamera2;
-	[SerializeField] private float speed = 5f;
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+    [SerializeField] private Camera playerCamera1;
+    [SerializeField] private Camera playerCamera2;
+    [SerializeField] private float speed = 5f;
 
-	private void FixedUpdate ()
-	{
-		MoveCheck();
-		ZoomCheck();
-	}
+    private void FixedUpdate()
+    {
+        MoveCheck();
+        ZoomCheck();
+    }
 
-	private void MoveCheck()
-	{
-		if (Input.GetKey(KeyCode.A)) Movement(player1, -player1.right);
-		if (Input.GetKey(KeyCode.D)) Movement(player1, player1.right);
-		if (Input.GetKey(KeyCode.W)) Movement(player1, player1.forward);
-		if (Input.GetKey(KeyCode.S)) Movement(player1, -player1.forward);
-		if (Input.GetKey(KeyCode.LeftArrow)) Movement(player2, -player2.right);
-		if (Input.GetKey(KeyCode.RightArrow)) Movement(player2, player2.right);
-		if (Input.GetKey(KeyCode.UpArrow)) Movement(player2, player2.forward);
-		if (Input.GetKey(KeyCode.DownArrow)) Movement(player2, -player2.forward);
-	}
+    private void MoveCheck()
+    {
+        var moveAD = Input.GetAxis("HorizontalWASD");
+        var moveSW = Input.GetAxis("VerticalWASD");
+        var moveLeftRight = Input.GetAxis("HorizontalArrow");
+        var moveDownUp = Input.GetAxis("VerticalArrow");
 
-	private void ZoomCheck()
-	{
-		var fow = playerCamera1.fieldOfView;
-		if (Input.GetMouseButton(1))
-		{
-			if (!(fow > 40)) return;
-			playerCamera1.fieldOfView -= 1f;
-			playerCamera2.fieldOfView -= 1f;
-		}
-		else
-		{
-			if (!(fow < 60)) return;
-			playerCamera1.fieldOfView += 1f;
-			playerCamera2.fieldOfView += 1f;
-		}
-	}
+        if (moveAD != 0 || moveSW != 0)
+            Movement(player1, new Vector3(moveAD, 0, moveSW));
+        if (moveLeftRight != 0 || moveDownUp != 0)
+            Movement(player2, new Vector3(moveLeftRight, 0, moveDownUp));
+    }
 
-	private void Movement(Transform player, Vector3 dir)
-	{
-		player.position += dir * (speed * Time.fixedDeltaTime);
-	}
+    private void ZoomCheck()
+    {
+        var fow = playerCamera1.fieldOfView;
+        if (Input.GetMouseButton(1))
+        {
+            if (!(fow > 40)) return;
+            playerCamera1.fieldOfView -= 1f;
+            playerCamera2.fieldOfView -= 1f;
+        }
+        else
+        {
+            if (!(fow < 60)) return;
+            playerCamera1.fieldOfView += 1f;
+            playerCamera2.fieldOfView += 1f;
+        }
+    }
+
+    private void Movement(Component player, Vector3 dir)
+    {
+        player.transform.Translate(dir * (speed * Time.fixedDeltaTime));
+    }
 }
